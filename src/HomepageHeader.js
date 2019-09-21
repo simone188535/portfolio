5c7b408c7f56534c5b8412ec5c268bf73bea6bec
Simone Anthony <santhony@PR1-DM-SimoneAnthony.attlocal.net>
2019-09-20 21:09:25 -0400

    jumbotron init

---
+++ src/HomepageHeader.js
@@ -0,0 +1,21 @@
+import React from 'react';
+
+const HomepageHeader = () => {
+
+    return (
+        <div className="row">
+            <div className=" col-12 bg-primary vh-100" >
+                <div className="container">
+                    <div className="text-center">
+                        <div className="text-uppercase">The portfolio of</div>
+                        <div>Simone</div>
+                        <div>Anthony</div>
+                        <div>Full Stack Web Developer</div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+}
+
+export default HomepageHeader;
