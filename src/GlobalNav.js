2e3f458a078383e4d142257561adbb304c730d3e
Simone Anthony <santhony@PR1-DM-SimoneAnthony.local>
2019-09-24 14:37:22 -0400

    gsap is installed and working

--- src/GlobalNav.js
+++ src/GlobalNav.js
@@ -1,4 +1,4 @@
-import React, { Component, createRef } from 'react';
+import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
 
 class GlobalNav extends Component {
--- src/Homepage.js
+++ src/Homepage.js
@@ -1,35 +1,45 @@
-import React, { Component, useRef,useEffect } from 'react';
-// import stockImage from './assets/m-n-TgAqZAd9InA-unsplash.jpg';
-import { Tween, Power3 } from 'react-gsap';
-// import logo from './logo.png';
+import React, { Component} from 'react';
+import {TweenMax, TweenLite} from "gsap/TweenMax";
+// import { TimelineMax, CSSPlugin, ScrollToPlugin, Draggable } from "gsap/all";
 
-// class Homepage extends Component {
-    const Homepage = () => {
+
+class Homepage extends Component {
+    // const Homepage = () => {
+    constructor(){
+        super();
+        // this.circleToBG = React.createRef();
+        //ref to dom node
+        this.jumboHeadContainer = null;
+    }
 
     // simpleRef = createRef();
-    let logoItem = useRef(null);
+    // let logoItem = useRef(null);
     
-    console.log(logoItem);
-    // componentDidMount(){
-
-    // }
-    // render() {
-        useEffect(()=>{
-            console.log(logoItem);
-        });
+    // console.log(logoItem);
+    headerText = () => {
+        TweenLite.to(this.jumboHeadContainer, 1, {x: 100, y: 100});
+    }
+    componentDidMount(){
+        this.headerText();
+    }
+    render() {
+        // useEffect(()=>{
+        //     console.log(logoItem);
+        // });
+        
         return (
             <div className="home">
                 <div className="container-fluid">
-                    <div className="row jumbo-head section-one ">
+                    <div className="row jumbo-head section-one" >
                         <div className="col-md-6 vh-100 primary-background position-relative">
-                            <div className="text-align-left position-absolute header-text-container">
+                            <div className="text-align-left position-absolute header-text-container"  ref={div => this.jumboHeadContainer = div}>
                                 <div className="header-thin primary-dark-color headline-thin">Hello. My name is ...</div>
                                 <h1 className="header-medium white headline-med">Simone Anthony</h1>
                                 <div className="header-medium primary-dark-color headline-med">Full Stack Web Developer.</div>
                             </div>
                         </div>
                         <div className="col-md-6 vh-100 secondary-background position-relative">
-                            <div id="black-circle-to-bg" className="position-absolute">
+                            <div id="black-circle-to-bg" className="position-absolute" ref={this.circleToBG}>
                             </div>
                                 <div id="light-header-box" className="header-box position-absolute"></div>
                                 <div id="mid-header-box" className="position-absolute"></div>
@@ -53,24 +63,11 @@ import { Tween, Power3 } from 'react-gsap';
                             <h2 className="text-center">Still Learning</h2>
                         </div>
                     </div>
-                    {/* <div className="row">
-                            <div className="offset-md-7 col-md-5 position-relative px-0">
-                                <img className="w-100 img-fluid header-img" src={stockImage} alt="concrete building"/>
-                            </div>
-                        </div>
-                        <div className="row">
-                            <div className="col-md-7 position-relative">
-                            <div className="text-align-left position-absolute header-text-container">
-                                <div className="header-thin primary-dark-color headline-thin">Hello. My name is ...</div>
-                                <h1 className="header-medium primary-color-light headline-med">Simone Anthony</h1>
-                                <div className="header-medium primary-dark-color headline-med">Full Stack Web Developer.</div>
-                            </div>
-                            </div>
-                        </div> */}
+
                 </div>
             </div>
         );
-    // }
+    }
 }
 
 export default Homepage;
