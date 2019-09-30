5e9a65177d92459daad4907508fb9c2f6c0152ab
Simone Anthony <santhony@PR1-DM-SimoneAnthony.local>
2019-09-30 11:28:04 -0400

    edits made to stagger cards. Stable

--- src/js/props-objects/skills-props.js
+++ src/js/props-objects/skills-props.js
@@ -1,33 +1,43 @@
 const skills = [
     {
         img: 'https://dgpr.s3.amazonaws.com/images/camera-icon-png-18.jpg',
-        description: 'test',
-        alt: 'test'
+        description: 'React',
+        alt: 'React'
     },
     {
         img: 'https://dgpr.s3.amazonaws.com/images/camera-icon-png-18.jpg',
-        description: 'test2',
-        alt: 'test2'
+        description: 'HTML5',
+        alt: 'HTML5'
     },
     {
         img: 'https://dgpr.s3.amazonaws.com/images/camera-icon-png-18.jpg',
-        description: 'test3',
-        alt: 'test3'
+        description: 'CSS3',
+        alt: 'CSS3'
     },
     {
         img: 'https://dgpr.s3.amazonaws.com/images/camera-icon-png-18.jpg',
-        description: 'test4',
-        alt: 'test4'
+        description: 'Sass',
+        alt: 'Sass'
     },
     {
         img: 'https://dgpr.s3.amazonaws.com/images/camera-icon-png-18.jpg',
-        description: 'test5',
-        alt: 'test5'
+        description: 'PHP',
+        alt: 'PHP'
     },
     {
         img: 'https://dgpr.s3.amazonaws.com/images/camera-icon-png-18.jpg',
-        description: 'test6',
-        alt: 'test6'
+        description: 'MySQL',
+        alt: 'MySQL'
+    },
+    {
+        img: 'https://dgpr.s3.amazonaws.com/images/camera-icon-png-18.jpg',
+        description: 'Bootstrap 3/4',
+        alt: 'Bootstrap 3/4'
+    },
+    {
+        img: 'https://dgpr.s3.amazonaws.com/images/camera-icon-png-18.jpg',
+        description: 'Wordpress',
+        alt: 'Wordpress'
     }
 ];
 
