da882107f89a1b7e6799a27636221bb41d68b65f
Simone Anthony <santhony@PR1-DM-SimoneAnthony.attlocal.net>
2019-10-06 14:18:01 -0400

    background hero about me init

--- src/about/About.js
+++ src/about/About.js
@@ -1,6 +1,24 @@
 import React from 'react';
 
 const About = () => {
-    return (<div>About</div>);
+    return (
+        <div className="about">
+            <div className="container-fluid ">
+                <div className="row">
+                    <div className="col-12 px-0">
+                            <div className="hero-section w-100">
+                            <div className="hero-opac">
+                                <h1>About Me</h1>
+                            </div>
+                        </div>
+                        {/* <h1>About Us</h1>
+                        <h1>About Us</h1>
+                        <h1>About Us</h1>
+                        <h1>About Us</h1> */}
+                    </div>
+
+                </div>
+            </div>
+        </div>);
 }
 export default About;
