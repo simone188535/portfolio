edcfca5cc691f758e2ae10f9820393f5d24ce109
Simone Anthony <santhony@PR1-DM-SimoneAnthony.local>
2019-10-07 15:21:23 -0400

    css edits to about me

--- src/about/About.js
+++ src/about/About.js
@@ -5,16 +5,23 @@ const About = () => {
         <div className="about">
             <div className="container-fluid ">
                 <div className="row">
-                    <div className="col-12 px-0">
+                    <div className="col-md-3 px-0 left-hero ">
                             <div className="hero-section w-100">
-                            <div className="hero-opac">
-                                <h1>About Me</h1>
+                            <div className="h-100">
+                                <h1 className="hero-text hero-text-left position-relative">
+                                <div className="header-medium">About</div>
+                                <div className="header-medium">Me</div>
+                                </h1>
                             </div>
                         </div>
-                        {/* <h1>About Us</h1>
-                        <h1>About Us</h1>
-                        <h1>About Us</h1>
-                        <h1>About Us</h1> */}
+                    </div>
+                    <div className="col-md-9 px-0 right-hero ">
+                        <div className="hero-section w-100">
+                            <div className="hero-text hero-text-right-top position-relative">
+                            "I am a web developer who wants to create exciting Projects. "
+                            </div>
+                            <div className="hero-text hero-text-right-bottom position-relative">I'd prefer to use React but, Im just as adequate (if not more) with the other technologies ive learned</div>
+                        </div>
                     </div>
 
                 </div>
