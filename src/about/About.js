e60aff60b7596ca43e3b288dc7c043a27729f1e6
Simone Anthony <santhony@PR1-DM-SimoneAnthony.attlocal.net>
2019-10-08 16:39:02 -0400

    about hero animation complete

--- src/about/About.js
+++ src/about/About.js
@@ -1,5 +1,5 @@
 import React, { useEffect } from 'react';
-// import { TimelineLite,TweenMax} from "gsap/TweenMax";
+import { TimelineLite } from "gsap/TweenMax";
 import $ from 'jquery';
 import { isInViewport } from '../js/isInViewport';
 import SubHeadComponent from '../components/SubHeadComponent';
@@ -8,15 +8,31 @@ import SubHeadComponent from '../components/SubHeadComponent';
 
 const About = () => {
 
-    const aboutHeroAnimation = (heroIdent1) => {
-        
+    const aboutHeroAnimation = (heroIdent1,heroIdent2) => {
+
+        const tl = new TimelineLite();
         if (isInViewport($(heroIdent1))) {
             // heroLeftAnimation();
-            console.log('animation');
+
+            // tl.to($('#bottom-to-top'), .5, {
+            //     height: "100%"
+            // })
+           tl.to($('#hero-text-left'), 1, {
+                opacity: 1
+            });
+            
+        }
+        if (isInViewport($(heroIdent2))) {
+            tl.to($('#top-to-bottom'), .5, {
+                height: "100%"
+            })
+            .to($('#hero-text-right'), .5, {
+                opacity: 1
+            });
         }
     }
     const allAnimation = () => {
-        aboutHeroAnimation();
+        aboutHeroAnimation('#left-hero-content','#right-hero-content');
     }
     const SubHeadTrigger = {
         ident: 'aboutTrigger1',
@@ -28,38 +44,45 @@ const About = () => {
         allAnimation();
         window.addEventListener('scroll', allAnimation);
         // console.log('active');
-    
+
     });
-    
+
     return (
         <div className="about">
             <div className="container-fluid ">
                 <div className="row">
                     <div className="col-md-3 px-0 left-hero ">
-                            <div className="hero-section w-100">
-                            
-                            <div className="h-100">
-                                <h1 className="hero-text hero-text-left position-relative">
-                                <div className="header-medium">About</div>
-                                <div className="header-medium">Me</div>
-                                </h1>
-                            </div>
+                        <div className="hero-section w-100 position-relative">
+
+                            <div className="h-100" id="left-hero-content">
+                                {/* <div id="bottom-to-top" className="position-absolute w-100"> */}
+                                    <div id="hero-text-left" className="hero-text  position-relative">
+                                        <div className="header-medium">About</div>
+                                        <div className="header-medium">Me</div>
+                                    </div>
+                                </div>
+                            {/* </div> */}
 
                         </div>
                     </div>
                     <div className="col-md-9 px-0 right-hero ">
-                        <div className="hero-section w-100">
-                            
-                            <div className="hero-text hero-text-right-top position-relative">
-                            "I am a web developer who wants to create exciting Projects. "
+                        <div className="hero-section w-100 position-relative">
+                            <div className="h-100" id="right-hero-content">
+                                <div id="top-to-bottom" className="position-absolute w-100">
+                                    <div id="hero-text-right" className="position-relative">
+                                        <div className="hero-text hero-text-right-top position-relative">
+                                            "I am a web developer who wants to create exciting Projects. "
+                                </div>
+                                        <div className="hero-text hero-text-right-bottom position-relative">I'd prefer to use React but, Im just as adequate (if not more) with the other technologies ive learned</div>
+                                    </div>
+                                </div>
                             </div>
-                            <div className="hero-text hero-text-right-bottom position-relative">I'd prefer to use React but, Im just as adequate (if not more) with the other technologies ive learned</div>
-                        
+
                         </div>
                     </div>
 
                 </div>
-                <SubHeadComponent {...SubHeadTrigger}/>
+                <SubHeadComponent {...SubHeadTrigger} />
             </div>
         </div>);
 }
