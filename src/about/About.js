18661945625371158b5500b22ad1b8a69c9e21cc
Simone Anthony <santhony@PR1-DM-SimoneAnthony.attlocal.net>
2019-10-11 11:20:48 -0400

    work hero init

--- src/about/About.js
+++ src/about/About.js
@@ -26,9 +26,11 @@ const About = () => {
         if (isInViewport($(heroIdent2))) {
             tl.to($('#top-to-bottom'), .5, {
                 height: "100%"
+
             }, "-=1")
                 .to($('#hero-text-right'), .5, {
-                    opacity: 1
+                    opacity: 1,
+                    y:-10
                 });
         }
     }
--- src/work/Work.js
+++ src/work/Work.js
@@ -1,4 +1,9 @@
 import React, { useEffect } from 'react';
+import { TimelineLite } from "gsap/TweenMax";
+import $ from 'jquery';
+import { isInViewport } from '../js/isInViewport';
+import SubHeadComponent from '../components/SubHeadComponent';
+
 
 const Work = () => {
 
@@ -6,6 +11,11 @@ const Work = () => {
     const allAnimation = () => {
         console.log('working');
     }
+    const workTrigger = {
+        ident: 'workTrigger',
+        text: 'Previous Clients'
+
+    };
     useEffect(() => {
         allAnimation();
         window.addEventListener('scroll', allAnimation);
@@ -20,12 +30,21 @@ const Work = () => {
                     <div className="col-12 bg-red-orange">
                         <div className="hero-section w-100">
                             <div className="h-100" id="hero-content">
-                                <div className="hero-text position-relative">
-                                    Work
+                                <div className="hero-text position-relative h-100">
+                                    <div className="hero-text-container position-absolute"><div className="float-left">Work</div><div className="float-left">.</div></div>
+                                    {/* <div id="scaling-period" className=""></div> */}
                                 </div>
+
+
                             </div>
                         </div>
                     </div>
+
+                </div>
+            </div>
+            <div className="container">
+                <SubHeadComponent {...workTrigger} />
+                <div className="row">
 
                 </div>
             </div>
