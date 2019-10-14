812a4f9786f9b8df4a190cea280fc0a6d309242e
Simone Anthony <santhony@PR1-DM-SimoneAnthony.attlocal.net>
2019-10-13 23:36:48 -0400

    Updates

---
+++ precache-manifest.44df36b826c0483799ce655009e928de.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f6613686ee41b97e24d31652777c1fbb",
+    "url": "/index.html"
+  },
+  {
+    "revision": "687ca59e2aa4b713f1d9",
+    "url": "/static/css/main.19350fe4.chunk.css"
+  },
+  {
+    "revision": "944adad9663be905685d",
+    "url": "/static/js/2.ef2369a7.chunk.js"
+  },
+  {
+    "revision": "687ca59e2aa4b713f1d9",
+    "url": "/static/js/main.3a6d35f3.chunk.js"
+  },
+  {
+    "revision": "3c2210b84b35e0ca614f",
+    "url": "/static/js/runtime-main.7302273d.js"
+  },
+  {
+    "revision": "b9ca8268e93605ca52510ed9da37cd42",
+    "url": "/static/media/Resume.b9ca8268.pdf"
+  }
+]);
