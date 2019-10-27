af3be2d722a2a458869420ae4f909336075d001c
Simone Anthony <santhony@PR1-DM-SimoneAnthony.attlocal.net>
2019-10-26 22:57:32 -0400

    all projects organized and pit into object

--- src/js/props-objects/project-props.js
+++ src/js/props-objects/project-props.js
@@ -1,10 +1,57 @@
 const projects = [
     {
         id:1,
-        img:'',
-        alt:'',
-        description:'',
-        'link':''
+        img:'/images/logos/DGWesternUnion.png',
+        alt:'Dollar General Western Union',
+        title:'Dollar General Western Union',
+        description:'This is a landing built with HTML, CSS, JQuery and Bootstrap3',
+        'link': 'https://www.dollargeneral.com/westernunion',
+        'github':'https://github.com/simone188535/dollar-general-western-union'
+    },
+    {
+        id:2,
+        img:'/images/logos/searshometownstores2.png',
+        alt:'Sears Landing PHP Template',
+        title:'Sears Landing PHP Template',
+        description:' This is a PHP template built for creating landing pages quickly',
+        'link':'http://sb6.hometown.kili.netsuitestaging.com/how-to-clean-your-oven',
+        'github': 'https://github.com/simone188535/sears_repo'
+    },
+    {
+        id:3,
+        img:'/images/logos/caymus-equity.png',
+        alt:'Caymus Equity',
+        title:'Caymus Equity',
+        description:'This is a Wordpress site I&39;ve helped maintain.',
+        'link':'http://caymusequity.com/',
+        'github': 'https://github.com/simone188535/caymus-repo'
+    },
+    {
+        id:4,
+        img:'/images/logos/KFEmail.png',
+        alt:'Email Template',
+        title:'Email Template',
+        description:'Responsive Email Template built in Ruby that I helped modify modifiy maintain',
+        'link':null,
+        'github': 'https://github.com/simone188535/email_template'
+    },
+    {
+        id:5,
+        img:'/images/logos/DGApp.png',
+        alt:'Dollar General App Page',
+        title:'Dollar General App Page',
+        description:'This is another Dollar General Landing Page',
+        'link':'https://www.dollargeneral.com/app.html',
+        'github': 'https://github.com/simone188535/dollar-general-app'
+    },
+    {
+        id:6,
+        img:'/images/logos/react.png',
+        alt:'Simple React todo List',
+        title:'Simple React todo List',
+        description:'This is an application that uses React and Redux',
+        'link':null,
+        'github': 'https://github.com/simone188535/react-todo-list'
     },
 
 ]
